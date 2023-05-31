f4ad4b024ceea53da2f9eb6d3dba6f7a666e989a
Victor Garcia <vgarcia@aidacanarias.com>
2023-05-31 17:05:23 +0200

    Refactor: Extracted IsAnyPlayerInAdvatangeOrWinningSituation adn GetAdvantageOrWinningScore

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -29,13 +29,9 @@ namespace Tennis
             {
                 score = GetDrawScore();
             }
-            else if (m_score1 >= 4 || m_score2 >= 4)
+            else if (IsAnyPlayerInAdvatangeOrWinningSituation())
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = GetAdvantageOrWinningScore();
             }
             else
             {
@@ -63,6 +59,23 @@ namespace Tennis
             return score;
         }
 
+        private bool IsAnyPlayerInAdvatangeOrWinningSituation()
+        {
+            return m_score1 >= 4 || m_score2 >= 4;
+        }
+
+        private string GetAdvantageOrWinningScore()
+        {
+            var score = (m_score1 - m_score2) switch
+            {
+                1 => "Advantage player1",
+                -1 => "Advantage player2",
+                >= 2 => "Win for player1",
+                _ => "Win for player2"
+            };
+            return score;
+        }
+
         private bool IsDraw()
         {
             return m_score1 == m_score2;
@@ -70,8 +83,7 @@ namespace Tennis
 
         private string GetDrawScore()
         {
-            string score;
-            score = m_score1 switch
+            var score = m_score1 switch
             {
                 0 => "Love-All",
                 1 => "Fifteen-All",
